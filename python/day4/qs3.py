c1555f8b4e853dbad0b9d73829ac189e37f98420
ppawan388 <65589103+ppawan388@users.noreply.github.com>
2020-09-07 12:38:11 +0530

    Add files via upload

---
+++ python/day4/qs3.py
@@ -0,0 +1,9 @@
+print("enter two numbers")
+x=int(input())
+y=int(input())
+sub=x-y
+print("result",sub)
+if sub>25:
+	print(x*y)
+else:
+	print(x/y)
---
+++ python/day4/qs4.py
@@ -0,0 +1,9 @@
+import math
+lst =[4,85,98,7,45,12,36,47,51,48,65,15]
+print(lst)
+for i in lst:
+	if i%2==0:
+		result=math.sqrt(i)-2
+		print(result)
+
+
