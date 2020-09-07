9718d279baed78feb5415257e5cc58c8b110c10d
ppawan388 <65589103+ppawan388@users.noreply.github.com>
2020-09-07 12:40:09 +0530

    Update qs1.py

--- python/day4/qs1.py
+++ python/day4/qs1.py
@@ -1,1 +1,19 @@
+x1=int(input())
+y1=int(input())
+x2=int(input())
+y2=int(input())
+z1=complex(x1,y1)
+z2=complex(x2,y2)
+add= z1+z2
+print("addition",add)
+sub=z1-z2
+print("subtraction",sub)
+mul=z1*z2
+print("multipication",mul)
+div=z1/z2
+print("division",div)
+#floor=z1//z2
+#print("floor division",floor)
+#modulo=z1%z2
+#print("modulo",modulo)
 
