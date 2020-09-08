3af5278b9663a3d7f29c4515a22866b2d8b40722
ppawan388 <65589103+ppawan388@users.noreply.github.com>
2020-09-08 16:41:17 +0530

    Update qs1.py

--- python/day5/qs1.py
+++ python/day5/qs1.py
@@ -1,1 +1,19 @@
+import math
+def is_prime(num):
+	if num<=1:
+		return false
+	div=math.floor(math.sqrt(num))
+	for i in range(2,div+1):
+		if num%i==0:
+			return False
+	return True
+num=3
+count=0
+while(count!=20):
+	result=is_prime(num)
+	if result==True:
+		print(num)
+		count=count+1
+		
+	num+=1
 
