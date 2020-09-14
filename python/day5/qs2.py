2c07d99cd380511dc5a571966039c41122a0876a
ppawan388 <ppawan388@gmail.com>
2020-09-14 17:41:52 +0530

    done

---
+++ python/day5/qs2.py
@@ -0,0 +1,14 @@
+						str=" Hello World  "
+
+"""print(max(str))
+print(str.isspace())
+print("length",len(a))
+print("strip removes whitespaces from start and end of string",str.strip())
+print("lower case",str.lower())
+print("upper case",str.upper())
+print("replace",str.replace("H","J"))
+print("split",str.split(","))
+print("count",str.count("r"))
+print("center",str.center(15))"""
+print("encoding ",str.lower())
+
---
+++ python/day5/qs3.py
@@ -0,0 +1,6 @@
+print("enter the string to check whether it is palindrome:")
+str=str(input())
+rev_str=str[::-1]
+print(rev_str)
+#print(str)
+
